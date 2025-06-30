8b6f21c6033e8e0832b35e20e0c2a19cbf0255ae
aidengseay <aidengseay@gmail.com>
2025-06-30 16:33:34 -0700

    finished database implementation

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config({ path: "./config.env" });
 import pg from "pg";
+import bcrypt from "bcrypt";
 
 // constants ///////////////////////////////////////////////////////////////////
 
 const app = express();
 const port = 3000;
 const openWeatherKey = process.env.OPEN_WEATHER_KEY;
+const saltRounds = 10;
 
 const db = new pg.Client({
     user: process.env.DB_USER,
@@ -24,18 +26,10 @@ const db = new pg.Client({
 
 // structures //////////////////////////////////////////////////////////////////
 
-function BlogPost(author, title, category, content) {
-    this.author = author;
-    this.title = title;
-    this.category = category;
-    this.content = content
-    this.time = new Date().toLocaleString("en-US");
-}
-
-let blogPosts = [];
 let categoryFilter = "None";
-let user = null;
-// ERROR: there can only be one user per session
+let userId = null;
+let displayName = null;
+// WARNING: there can only be one user per session
 
 // middleware //////////////////////////////////////////////////////////////////
 
@@ -45,38 +39,47 @@ db.connect();
 
 // page requests ///////////////////////////////////////////////////////////////
 
-// render the home page
-app.get("/", (req, res) => {
+// render the home page (blog)
+app.get("/", async (req, res) => {
 
-    res.render("index.ejs", {blogPosts, categoryFilter, user});
+    try {
+
+        // get all blogs from database
+        const blogPostsResponse = await db.query("SELECT * FROM blogs");
+        res.render("index.ejs", {blogPosts : blogPostsResponse.rows, categoryFilter, userId});
+    
+    // display any errors
+    } catch (err) {
+        console.error("error executing query", err);
+    }
 
 });
 
 // render the home page by link
 app.post("/", (req, res) => {
 
-    res.render("index.ejs", {blogPosts, categoryFilter, user});
+    res.redirect("/");
 
 });
 
 // render sign in page
 app.get("/signin", (req, res) => {
 
-    res.render("signin.ejs", {user});
+    res.render("signin.ejs", {userId});
 
 });
 
 // render sign up page
 app.get("/signup", (req, res) => {
 
-    res.render("signup.ejs", {user});
+    res.render("signup.ejs", {userId});
 
 });
 
 // render account page
 app.get("/manage-account", (req, res) => {
 
-    res.render("account.ejs", {user});
+    res.render("account.ejs", {userId});
 
 });
 
@@ -85,33 +88,35 @@ app.get("/manage-account", (req, res) => {
 // creates an new account and checks for duplicate usernames
 app.post("/create-account", async (req, res) => {
 
-    const username = req.body["username"];
-    const password = req.body["password"];
-    const displayName = req.body["disp-name"];
+    // get variables from ejs file
+    const newUsername = req.body["username"];
+    const newPassword = req.body["password"];
+    const newDisplayName = req.body["disp-name"];
 
     try {
 
         // check if username already taken
-        const existingUsers = await db.query("SELECT user_id FROM users WHERE user_id = $1", [username]);
+        const existingUsers = await db.query("SELECT user_id FROM users WHERE user_id = $1", [newUsername]);
 
         if (existingUsers.rows.length > 0) {
-            return res.render("signup.ejs", { error: "Username already taken", user });
+            return res.render("signup.ejs", { error: "Username already taken", userId });
         }
 
-        // TODO: encrypt the password
+        // encrypt the password
+        bcrypt.hash(newPassword, saltRounds, async (err, newHash) => {
 
-        // add user data to db
-        var query = "INSERT INTO users (user_id, password, name) VALUES ($1, $2, $3)";
-        var values = [username, password, displayName];
+            // add user data to db
+            await db.query("INSERT INTO users (user_id, password, name) VALUES ($1, $2, $3)", [newUsername, newHash, newDisplayName]);
 
-        await db.query(query, values);
+            // go to sign-in page
+            res.render("signin.ejs", {userId}); 
+        })
 
-        res.render("signin.ejs", {user}); 
-
+    // catch any errors
     } catch (err) {
 
-        console.error("error executing query", err.stack);
-        res.render("signup.ejs", {user});
+        console.error("error executing query", err);
+        res.render("signup.ejs", {userId});
     }
 
 });
@@ -119,81 +124,125 @@ app.post("/create-account", async (req, res) => {
 // logs the user in when entering correct username and password
 app.post("/access-account", async (req, res) => {
 
-    const username = req.body["username"];
-    const password = req.body["password"];
+    // get variables from ejs file
+    const userIdAttempt = req.body["username"];
+    const passwordAttempt = req.body["password"];
 
-    // TODO: unencrypt the password
+    // get user information from username
+    const result = await db.query("SELECT * FROM users WHERE user_id = $1", [userIdAttempt]);
 
-    // check if valid username and password
-    var correctInformation = await db.query("SELECT user_id FROM users where user_id = $1 AND password = $2", [username, password]);
+    // check if user exists
+    if(result.rows.length > 0) {
 
-    if(correctInformation.rows.length === 0){
-        return res.render("signin.ejs", { error: "Incorrect username or password", user });
+        // unencrypt password
+        const hashedPassword = result.rows[0].password;
+        bcrypt.compare(passwordAttempt, hashedPassword, async (err, success) => {
+
+            // check if correct password
+            if(success) {
+
+                // set user account and render blog page
+                userId = userIdAttempt;
+
+                // get display name associated with the user id
+                try {
+                    const userResult = await db.query("SELECT name FROM users WHERE user_id = $1", [userId]);
+                    displayName = userResult.rows[0].name;
+
+                } catch (err) {
+                    console.error("error executing query", err);
+                }
+
+                res.redirect("/");
+            }
+
+            else {
+                res.render("signin.ejs", { error: "Incorrect username or password", userId });
+            }
+        });
+    } else {
+        res.render("signin.ejs", { error: "Incorrect username or password", userId });
     }
-
-    // set user account and render blog page
-    user = username;
-    res.render("index.ejs", {blogPosts, categoryFilter, user})
 });
 
 // sign the user out of the account
 app.post("/signout", (req, res) => {
 
-    user = null;
-    res.render("index.ejs", {blogPosts, categoryFilter, user});
-
-});
-
-// modify the display name and password
-app.post("/change-account", (req, res) => {
-
-    //TODO: change the display name and password (cannot change username)
-
-});
-
-// delete current account and all related posts
-app.post("/delete-account", (req, res) => {
-
-    // TODO: delete the account on the DB and their posts
+    userId = null;
+    displayName = null;
+    res.redirect("/");
 
 });
 
 // blog functions //////////////////////////////////////////////////////////////
 
 // make a new blog post
-app.post("/new", (req, res) => {
+app.post("/new", async (req, res) => {
+    
+    // insert new blog post into the database
+    try {
+        await db.query("INSERT INTO blogs (blog_id, creator_name, creator_user_id, title, body, date_created, category) VALUES (DEFAULT, $1, $2, $3, $4, $5, $6)",
+        [displayName, userId, req.body["title"], req.body["content"], new Date().toLocaleString("en-US"), req.body["category"]]);
 
-    var blogPost = new BlogPost(req.body["author"], req.body["title"],
-                                     req.body["category"], req.body["content"]);
-    blogPosts.push(blogPost);
+    } catch (err) {
+        console.log(err);
+    }
+
+    // refresh the blog page
     res.redirect("/");
 
 });
 
 // delete a blog post
-app.post("/delete", (req, res) => {
+app.post("/delete", async (req, res) => {
 
-    blogPosts.splice(Number(req.body["blogNum"]), 1);
+
+    // delete item in database
+    try {
+        await db.query("DELETE FROM blogs WHERE blog_id = $1", [req.body["blogId"]]);
+
+    } catch(err) {
+        console.error(err);
+    }
+
+    // refresh the home page
     res.redirect("/");
 
 });
 
 // edit a blog post
-app.post("/edit", (req, res) => {
+app.post("/edit", async (req, res) => {
 
-    var blogIndex = Number(req.body["blogNum"]);
-    res.render("edit.ejs", {blogPosts, blogIndex});
+    // get the default values of the blog being edited
+    try {
+
+        const editResponse = await db.query("SELECT * FROM blogs WHERE blog_id = $1", [req.body["blogId"]]);
+        const blogPost = editResponse.rows[0];
+
+        res.render("edit.ejs", {blogPost, userId});
+
+    } catch(err) {
+        console.error(err);
+    }
 
 });
 
 // update the blog post from edit
-app.post("/update", (req, res) => {
+app.post("/update", async (req, res) => {
 
-    blogPosts.splice(Number(req.body["blogNum"]), 1);
-    var blogPost = new BlogPost(req.body["author"], req.body["title"],
-                                     req.body["category"], req.body["content"]);
-    blogPosts.push(blogPost);
-    res.redirect("/");
+    // update the database with the new values
+    try {
+
+        await db.query("UPDATE blogs SET title = $1, category = $2, body = $3, date_created = $4 WHERE blog_id = $5",
+        [req.body["title"], req.body["category"], 
+        req.body["content"], new Date().toLocaleString("en-US"),  
+        req.body["blogId"]]);
+
+        res.redirect("/");
+        
+    } catch(err) {
+        console.error(err);
+    }
 
 });
 
@@ -228,10 +277,10 @@ app.post("/weather", async (req, res) => {
         const weatherDescription = result.weather[0].description;
         const weatherIcon = result.weather[0].icon;
 
-        res.render("weather.ejs", {temp, tempMin, tempMax, humidity, weatherMain, weatherDescription, weatherIcon, user});
+        res.render("weather.ejs", {temp, tempMin, tempMax, humidity, weatherMain, weatherDescription, weatherIcon, userId});
         
     } catch (error) {
-        res.render("weather.ejs", {error, user});
+        res.render("weather.ejs", {error, userId});
     }
 
 });
